38b7e383c1fde3c6a341d3f217fdc4777f37f0f9
Danillo Monteiro <danillo.monteiro324@outlook.com>
2023-05-29 19:25:25 -0300

    debug images

--- app/page.js
+++ app/page.js
@@ -1,5 +1,3 @@
-"use client";
-
 import Imagens from "@/src/components/imagens";
 import Text from "@/src/components/text";
 import Icons from "@/src/components/icons";
@@ -79,7 +77,78 @@ export default function Home() {
             </div>
           </div>
         </div>
-        <Imagens />
+        <Image
+          className="estrela-do-mar"
+          src="/../public/images/estrela-do-mar.png"
+          width={70}
+          height={70}
+        />
+        <Image
+          className="guarda-sol"
+          src="/../public/images/guarda-sol1.png"
+          width={170}
+          height={170}
+        />
+        <Image
+          className="concha-do-mar"
+          src="/../public/images/concha-do-mar.png"
+          width={50}
+          height={50}
+        />
+        <Image
+          className="folhas-de-coqueiro"
+          src="/../public/images/folhas-de-coqueiro5.png"
+          width={190}
+          height={190}
+        />
+        <Image
+          className="folhas-de-coqueiro2"
+          src="/../public/images/folhas-de-coqueiro5.png"
+          width={190}
+          height={190}
+        />
+        <Image
+          className="folhas-de-coqueiro3"
+          src="/../public/images/folhas-de-coqueiro5.png"
+          width={190}
+          height={190}
+        />
+        <Image
+          className="folhas-de-coqueiro4"
+          src="/../public/images/folhas-de-coqueiro5.png"
+          width={190}
+          height={190}
+        />
+        <Image
+          className="folhas-de-coqueiro5"
+          src="/../public/images/folhas-de-coqueiro5.png"
+          width={190}
+          height={190}
+        />
+        <Image
+          className="folhas-de-coqueiro6"
+          src="/../public/images/folhas-de-coqueiro5.png"
+          width={190}
+          height={190}
+        />
+        <Image
+          className="folhas-de-coqueiro7"
+          src="/../public/images/folhas-de-coqueiro5.png"
+          width={190}
+          height={190}
+        />
+        <Image
+          className="folhas-de-coqueiro8"
+          src="/../public/images/folhas-de-coqueiro5.png"
+          width={190}
+          height={190}
+        />
+        <Image
+          className="toalha"
+          src="/../public/images/toalha1.png"
+          width={130}
+          height={130}
+        />
         <Waves />
       </div>
     </>
